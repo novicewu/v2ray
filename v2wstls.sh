d71287682593925ec0c73a8f34d892e37577e10b
Novice Wu <novice.wu@gmail.com>
2019-02-07 14:40:36 +0800

    Update v2wstls.sh

--- v2wstls.sh
+++ v2wstls.sh
@@ -74,12 +74,13 @@ uuid=$(cat /proc/sys/kernel/random/uuid)
 
 echo -e "{
     \"log\": {
-                \"access\": \"/var/log/v2ray/access.log\",
-                \"error\": \"/var/log/v2ray/error.log\",
+    \"access\": \"/var/log/v2ray/access.log\",
+    \"error\": \"/var/log/v2ray/error.log\",
         \"loglevel\": \"info\"
     },
     \"inbound\": {
         \"port\": 10000,
+        \"listen\": \"127.0.0.1\",
         \"protocol\": \"vmess\",
         \"allocate\": {
             \"strategy\": \"always\"
@@ -89,19 +90,16 @@ echo -e "{
                 \"id\": \"$uuid\",
                 \"level\": 1,
                 \"alterId\": 64,
+                \"security\": \"chacha20-poly1305\"
             }]
         },
         \"streamSettings\": {
             \"network\": \"ws\",
+            \"wsSettings\": {
+                \"connectionReuse\": false,
+                \"path\": \"/phpmyadmin/\"
             }
-        \"sniffing\": {
-                \"enabled\": true,
-                \"destOverride\": [
-                        \"http\",
-                        \"tls\"
-                ]
         }
-
     },
     \"outbound\": {
         \"protocol\": \"freedom\",
@@ -117,7 +115,7 @@ echo -e "{
         \"settings\": {
             \"rules\": [{
                 \"type\": \"field\",
-                \"ip\": [\"geoip:private\"],
+                \"ip\": [\"0.0.0.0/8\", \"10.0.0.0/8\", \"100.64.0.0/10\", \"127.0.0.0/8\", \"169.254.0.0/16\", \"172.16.0.0/12\", \"192.0.0.0/24\", \"192.0.2.0/24\", \"192.168.0.0/16\", \"198.18.0.0/15\", \"198.51.100.0/24\", \"203.0.113.0/24\", \"::1/128\", \"fc00::/7\", \"fe80::/10\"],
                 \"outboundTag\": \"blocked\"
             }]
         }
